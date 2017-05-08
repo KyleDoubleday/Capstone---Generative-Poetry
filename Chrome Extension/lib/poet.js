46e5c117e8b0cbc55b8bc374f6658251182f8a16
KyleDoubleday <kdoubleday7@gmail.com>
2017-05-08 00:06:06 -0400

    Add files via upload
    Update of Chrome extensions, changes mostly to poet.js. Implemented different poetic forms: freeverse works, haiku works just needs to filter stopwords, rhyme is having trouble with line length and repeating lines. Poet.js has issue where finding the "phones" for a body of text takes a long time, about 50 words/sec or 15 secs for a 750 word article. Need to improve speed (sets?). Finally, formatting issues with spaces and newlines not being filtered out are popping up.

--- Chrome Extension/lib/poet.js
+++ Chrome Extension/lib/poet.js
@@ -1,7 +1,5 @@
-var pronouncing = require('pronouncing');
-
 function displayPoem() {
-  alert(writePoem());
+  alert(writePoem("rhyme"));
 }
 
 function getText() {
@@ -19,23 +17,23 @@ function getText() {
   return text;
 }
 
-function writePoem(){
+function writePoem(type){
 	var text = getText();
-	var textFilt = text.replace('\n',' ').replace('\r','').toLowerCase().replace('[a-z]\.','');     //filter non alpha-characters, make it all lowercase	
+	var textFilt = text.replace(/(\r\n|\n|\r)/gm," ").toLowerCase().replace(/[a-z]\./gm,"");     //filter non alpha-characters, make it all lowercase	
 	var textList = textFilt.split(" ");
-	var phones = new Array(), syllableCounts = new Array();
-	var i = 0; 
+	var phones = new Array(), syllableCounts = new Array(), output = new Array();
 		
-	alert("Finding phones");														// Get phonetics for each word
+	console.log("Finding phones");													// Get phonetics for each word
+	console.log(textList.length);
 	for(i = 0; i < textList.length; i++){
-		if (pronouncing.phonesForWord(textList[i])){									 
+		if (i%100 ==0){console.log(i);}
+		if (pronouncing.phonesForWord(textList[i])){		 
 			phones[i] = (pronouncing.phonesForWord(textList[i]))[0];
 		}
 		else{
 			phones[i] = ('0');
 		}
 	}
-	alert("Finding Sylls");
 	for(i = 0; i < phones.length; i++){                                       //Get syllables for each phonetic
 		if (phones[i] != '0'){
 			syllableCounts[i] = (pronouncing.syllableCount(phones[i]));
@@ -44,7 +42,97 @@ function writePoem(){
 			syllableCounts[i] = (0);
 		}
 	}
-	return phones; 
+	
+	if(type == "rhyme"){
+		var textSet = new Set(textList);
+		var rhymePairs = new Array();
+
+		while(rhymePairs.length < 16){                                       //Until there are 16 words, 8 rhyming pairs:
+			
+			var x = Math.floor((Math.random() * syllableCounts.length) + 1);                        //Pick a random word in the text
+			rhymeSet = new Set(pronouncing.rhymes(textList[x]));
+			var rhymeCouple = new Set( [...textSet].filter(y => rhymeSet.has(y)));
+			
+			var k = 0;
+			if(Array.from(rhymeCouple).length != 0){
+				while(textList.indexOf((Array.from(rhymeCouple))[k]) == -1){
+					k = k+1;
+				}
+				if(rhymePairs.indexOf((Array.from(rhymeCouple))[k]) == -1){  //If there is another word in the text that rhymes with it
+					rhymePairs.push(textList[x-2] + ' ' + textList[x-1] + ' ' + textList[x]);                          //Write the pair to the rhymePairs list
+					rhymePairs.push(textList[(textList.indexOf((Array.from(rhymeCouple))[k]))-2] + ' ' + textList[(textList.indexOf((Array.from(rhymeCouple))[k]))-1] + ' ' + textList[textList.indexOf((Array.from(rhymeCouple))[k])]);
+				}
+			}
+		}
+		console.log(rhymePairs);
+		for(i = 0; i < 8; i++){        
+			var lineSyllablesPair = Math.floor((Math.random() * 7) + 4);               //Make them a random syllable length (can change this later)***
+			console.log("Linesyllables Pair = " + lineSyllablesPair);
+			for(j = 0; j < 2; j++){                                     //For 16 lines:
+				var lineSyllables = lineSyllablesPair;
+				var index = Math.floor((Math.random() * textList.length) + 1);                       //Fill lines with random words
+				while(lineSyllables > 0){  
+					console.log(lineSyllables);				
+					if((syllableCounts[index] != 0) && (syllableCounts[index] != "undefined")){  
+						console.log("Syllable of word is " + syllableCounts[index])
+						output.push(' ' + textList[index]);
+						lineSyllables = lineSyllables - syllableCounts[index];
+					}
+					index = index + 1;
+				}
+				output.push(' ' + rhymePairs[(2*i)+j]);                           //End lines on rhyme pairs, make into stanzas
+				output.push('\n');
+			}
+			if((i+1) % 2 == 0){
+				output.push('\n');
+			}
+		}
+	}
+	else if(type == "haiku"){
+		var lineSyllables;
+		for(i = 0; i < 3; i++){                                             //For 3 lines with 5,7,5 syllables:
+			if (i == 1){
+				lineSyllables = 7;
+			}
+			else{
+				lineSyllables = 5;
+			}
+			var index = Math.floor((Math.random() * syllableCounts.length) + 1);            //Fill line with random words until syllables are full.
+			while (lineSyllables > 0){
+				if(syllableCounts[index] <= lineSyllables && syllableCounts[index] != 0){
+					if (output.indexOf(textList[index]) == -1){// & textList[index] not in stop_word_set){  //If the word chosen is non-stop word, 
+						output.push(textList[index] + ' ');                                      // Use it to start a phrase
+						lineSyllables = lineSyllables - syllableCounts[index];
+					}
+					else if(index % 3 == 0){                               // accept non-stop words 1/3 of the time
+						output.push(textList[index] + ' ');
+						lineSyllables = lineSyllables - syllableCounts[index];
+					}
+					index = index + 1; 
+				}
+				else{
+					index = index = Math.floor((Math.random() * syllableCounts.length) + 1); 
+				}
+			}
+			output.push('\n');
+		}
+	}
+	else if(type == "freeverse"){
+		for (i = 2; i < Math.floor((Math.random() * 20) + 1); i++){                                       //For random number of lines:
+			var lineLength = Math.floor((Math.random() * 9) + 1);                              //Make them a random WORD length (can change this later)***
+			var wordCount = 0;
+			var index = Math.floor((Math.random() * syllableCounts.length) + 1);                   //Fill lines with random words
+			while(wordCount < lineLength){
+				output.push(textList[index] + ' ');
+				wordCount = wordCount + 1;
+				index = index + 1;
+			}
+			output.push('\n');
+			if(Math.floor((Math.random() * 5) + 1) % 4 == 0){
+				output.push('\n');
+			}
+		}
+	}
+	return output.join(""); 
 }
 displayPoem();	
-
